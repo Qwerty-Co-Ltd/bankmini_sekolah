caff9e98f5c5ac3e168755b9b87e92d149b5f89b
Nabil Khoerul Rijal <nabilkhoerulrjl2002@gmail.com>
2021-11-05 20:38:27 +0700

    Merubah nama controller Bank_Mini menjadi System_Login

--- application/controllers/System_Login.php
+++ application/controllers/System_Login.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Bank_Mini extends CI_Controller {
+class System_Login extends CI_Controller {
 
 	public function index()
 	{
